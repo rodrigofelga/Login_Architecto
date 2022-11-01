dbcdb656470899cb00bc1d547d9cae52949fcec4
RODRIGO FELGA DE CASTRO <54859866+rodrigofelga@users.noreply.github.com>
2022-11-01 11:52:22 -0400

    Add files via upload

---
+++ Login_Architecto/tailwind.config.cjs
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+  ],
+  theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
+    
+    colors: {
+      transparent: 'transparent',
+     
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        400: '#7c7c8a',
+        200: '#c4c4cc',
+        100: '#e1e1e6',
+      },
+
+      'black': '#000',
+      'white': '#FFF',
+
+      'cyan-500': '#81d8f7',
+      'cyan-300': '#9be1fb'
+           
+    },
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif'
+      },
+    },
+  },
+  plugins: [],
+}
